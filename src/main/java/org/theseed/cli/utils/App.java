47dfc97bba2db4ac301798e9ea1e8cd02293309e
Bruce Parrello <bparrello@figresearch.com>
2021-11-29 08:51:20 -0600

    Moved rna utilities to their own module.

--- src/main/java/org/theseed/cli/utils/App.java
+++ src/main/java/org/theseed/cli/utils/App.java
@@ -5,30 +5,13 @@ import java.util.Arrays;
 import org.theseed.bin.utils.BinReportProcessor;
 import org.theseed.bin.utils.QzaReportProcessor;
 import org.theseed.bin.utils.XMatrixProcessor;
-import org.theseed.rna.utils.FpkmAllProcessor;
-import org.theseed.rna.utils.FpkmSummaryProcessor;
-import org.theseed.rna.utils.RnaCopyProcessor;
-import org.theseed.rna.utils.RnaCorrelationProcessor;
-import org.theseed.rna.utils.RnaMapProcessor;
-import org.theseed.rna.utils.RnaSeqProcessor;
-import org.theseed.rna.utils.SampleMetaFixProcessor;
-import org.theseed.rna.utils.SampleMetaProcessor;
 import org.theseed.utils.BaseProcessor;
 
 /**
  * Commands for utilities relating to CLI processing.
  *
- * fpkm			run jobs to convert FASTQ files to FPKM results
- * fpkmSummary	produce a summary file from FPKM results
- * fpkmAll		generate all of the standard RNA SEQ files
- * rnaCopy		copy RNA read files into PATRIC for processing by the FPKM commands
- * rnaSetup		update the sampleMeta.tbl file from the progress.txt and rna.production.tbl files
- * rnaProdFix	add production and density data to a sampleMeta.tbl file
- * rnaMaps		consolidate RNA maps from batch expression data runs
- * rnaCorr		determine the +/0/- correlation between genes in an RNA database
  * binReport	determine bin composition in a PATRIC workspace directory
  * xMatrix		convert bin reports into a classification matrix
- * bGroups		read an RNA database and output the group IDs, organized by Blattner number
  * gtoRoles		get proteins from genomes based on roles
  * qzaReport	determine bin composition in trimmed Amplicon database
  * updateMaster	update a master PATRIC database:  remove obsolete genomes and add the new ones
@@ -43,41 +26,14 @@ public class App
         BaseProcessor processor;
         // Determine the command to process.
         switch (command) {
-        case "fpkm" :
-            processor = new RnaSeqProcessor();
-            break;
-        case "fpkmsummary" :
-            processor = new FpkmSummaryProcessor();
-            break;
-        case "fpkmall" :
-            processor = new FpkmAllProcessor();
-            break;
-        case "rnaCopy" :
-            processor = new RnaCopyProcessor();
-            break;
-        case "rnaSetup" :
-            processor = new SampleMetaProcessor();
-            break;
-        case "rnaProdFix" :
-            processor = new SampleMetaFixProcessor();
-            break;
         case "binReport" :
             processor = new BinReportProcessor();
             break;
         case "qzaReport" :
             processor = new QzaReportProcessor();
             break;
-        case "rnaMaps" :
-            processor = new RnaMapProcessor();
-            break;
-        case "rnaCorr" :
-            processor = new RnaCorrelationProcessor();
-            break;
         case "xmatrix" :
             processor = new XMatrixProcessor();
-            break;
-        case "bGroups" :
-            processor = new BlattnerProcessor();
             break;
         case "gtoRoles" :
             processor = new GtoRolesProcessor();
